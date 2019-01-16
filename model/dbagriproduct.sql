db74d56e8e51c0b123eb1fb9cb9f7c51ea631f8e
Marcuz Corpuz <macoycorpuz@gmail.com>
2019-01-16 15:05:47 +0800

    database update

--- model/dbagriproduct.sql
+++ model/dbagriproduct.sql
@@ -1,79 +1,53 @@
-CREATE TABLE `users` (
-  `userId` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `number` varchar(11) NOT NULL,
-  `address` varchar(200) NOT NULL,
-  `url` varchar(500) NOT NULL,
-  `isActivated` bit DEFAULT b'0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `admin` (
-  `adminId` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `deals` (
-  `dealId` int(11) NOT NULL,
-  `buyerId` int(11) NOT NULL
-  `productId` int(11) NOT
-  `time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `messages` (
-  `messageId` int(11) NOT NULL,
-  `dealId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `content` text NOT NULL,
-  `time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `products` (
-  `productId` int(11) NOT NULL,
-  `sellerId` int(11) NOT NULL,
-  `productName` varchar(100) NOT NULL,
-  `description` varchar(300) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `price` real NOT NULL,
-  `location` varchar(200) NOT NULL,
-  `lat` varchar(50) NOT NULL,
-  `lng` varchar(50) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `productUrl` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userId`),
-  ADD UNIQUE KEY `UQ_Email` (`email`);
-
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`adminId`),
-  ADD UNIQUE KEY `UQ_ADMINEMAIL` (`email`);
-
-ALTER TABLE `deals`   
-  ADD PRIMARY KEY (`dealId`);
-
-ALTER TABLE `messages`   
-  ADD PRIMARY KEY (`messageId`);
-
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`productId`);
-
-ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-  
-ALTER TABLE `deals`
-  MODIFY `dealId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
-ALTER TABLE `messages`
-  MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-  
-ALTER TABLE `products`
-  MODIFY `productId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
+CREATE TABLE users (
+  userId int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(50) NOT NULL,
+  number varchar(11) NOT NULL,
+  address varchar(200) NOT NULL,
+  url varchar(500) NOT NULL,
+  isActivated bit NOT NULL,
+  PRIMARY KEY (userId),
+  UNIQUE (email)
+);
+CREATE TABLE admin (
+  adminId int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(50) NOT NULL,
+  PRIMARY KEY (adminId),
+  UNIQUE (email)
+);
+CREATE TABLE deals (
+  dealId int(11) NOT NULL AUTO_INCREMENT,
+  buyerId int(11) NOT NULL,
+  productId int(11) NOT NULL,
+  time datetime NOT NULL,
+  PRIMARY KEY (dealId)
+);
+CREATE TABLE messages (
+  messageId int(11) NOT NULL AUTO_INCREMENT,
+  dealId int(11) NOT NULL,
+  userId int(11) NOT NULL,
+  content text NOT NULL,
+  time datetime NOT NULL,
+  PRIMARY KEY (messageId)
+);
+CREATE TABLE products (
+  productId int(11) NOT NULL AUTO_INCREMENT,
+  sellerId int(11) NOT NULL,
+  productName varchar(100) NOT NULL,
+  description varchar(300) NOT NULL,
+  quantity int(11) NOT NULL,
+  price real NOT NULL,
+  location varchar(200) NOT NULL,
+  lat varchar(50) NOT NULL,
+  lng varchar(50) NOT NULL,
+  status varchar(50) NOT NULL,
+  productUrl varchar(500) NOT NULL,
+  PRIMARY KEY (productId)
+);
 INSERT INTO admin (name, email, password) VALUES ('admin', 'admin', 'admin')
+
+
 
